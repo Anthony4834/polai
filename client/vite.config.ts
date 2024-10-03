34e64cb2592aebc6e765c29eb2bb2a51d7120e86
anthony.griffin <anthony.griffin@workday.com>
2024-10-03 14:58:12 -0700

    client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/polai/',
   plugins: [react()],
   server: {
     host: true, // Set to true to listen on all local IPs,
